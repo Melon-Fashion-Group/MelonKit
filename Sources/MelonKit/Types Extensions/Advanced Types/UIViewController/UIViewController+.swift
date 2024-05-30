32b1a8f1b3cd40450a246098232cf209724c7b3a
Dmitry Novikov <nvkv.dmitry@gmail.com>
2024-05-30 14:38:56 +0300

    feat: add uikit live preview service

---
+++ Sources/MelonKit/Types Extensions/Advanced Types/UIViewController/UIViewController+.swift
@@ -0,0 +1,42 @@
+//
+//  UIViewController+.swift
+//  MelonKit
+//
+//  Created by Dimka Novikov on 30.05.2024.
+//  Copyright © 2024 Melon Fashion Group. All rights reserved.
+//
+
+
+// MARK: Import section
+
+import SwiftUI
+
+
+
+// MARK: - UIViewController+
+
+extension UIViewController {
+
+    // MARK: - Public methods
+
+    ///
+    /// A method that represents a subclass of UIViewController into a view supported by SwiftUI Canvas that implements the View protocol.
+    ///
+    /// Example:
+    ///
+    ///     #Preview(.init(describing: CustomViewController.self)) {
+    ///         CustomViewController()
+    ///             .livePreview()
+    ///     }
+    ///
+    /// - Authors: Dmitry Novikov · Melon Fashion Group
+    ///
+    /// - Bug: None
+    ///
+    /// - Returns: A view supported by SwiftUI Canvas that implements the View protocol.
+    ///
+    @available(iOS 16.0, *)
+    public func livePreview() -> some View {
+        UIViewControllerLivePreview(viewController: self)
+    }
+}
