ed5a14deb84982b8a6d2b6d277d59722748499ee
Dmitry Novikov <nvkv.dmitry@gmail.com>
2024-05-30 14:28:43 +0300

    feat: add frmwk spec service

---
+++ Sources/MelonKit/Dev Tools/Project Specifications/Framework Specification/Extensions/Structures/MLNFramework.Version.swift
@@ -0,0 +1,42 @@
+//
+//  MLNFramework.Version.swift
+//  MelonKit
+//
+//  Created by Dimka Novikov on 22.05.2024.
+//  Copyright © 2024 Melon Fashion Group. All rights reserved.
+//
+
+
+// MARK: Import section
+
+import Foundation
+
+
+
+// MARK: - MLNFramework.Version
+
+extension MLNFramework {
+
+    // MARK: - Public structures
+
+    ///
+    /// A structure describing the version of the framework as a major.minor.patch.
+    ///
+    public struct Version {
+
+        ///
+        /// A property that allows you to get the major version of the framework.
+        ///
+        public let major: UInt16
+
+        ///
+        /// A property that allows you to get the minor version of the framework.
+        ///
+        public let minor: UInt16
+
+        ///
+        /// A property that allows you to get the patch version of the framework.
+        ///
+        public let patch: UInt16
+    }
+}
---
+++ Sources/MelonKit/Dev Tools/Project Specifications/Framework Specification/Typealiases/MLNFramework.Build.swift
@@ -0,0 +1,26 @@
+//
+//  MLNFramework.Build.swift
+//  MelonKit
+//
+//  Created by Dimka Novikov on 22.05.2024.
+//  Copyright © 2024 Melon Fashion Group. All rights reserved.
+//
+
+
+// MARK: Import section
+
+import Foundation
+
+
+
+// MARK: - MLNFramework.Build
+
+extension MLNFramework {
+
+    // MARK: - Public typealiases
+
+    ///
+    /// A type alias that describes the framework build version.
+    ///
+    public typealias Build = UInt16
+}
