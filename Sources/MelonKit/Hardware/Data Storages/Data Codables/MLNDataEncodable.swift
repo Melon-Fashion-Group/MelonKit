7ab73f80abfb056ecbf1e3a287c9c888950740c0
Dmitry Novikov <nvkv.dmitry@gmail.com>
2024-12-20 13:53:31 +0300

    feat: add data codables

---
+++ Sources/MelonKit/Hardware/Data Storages/Data Codables/MLNDataEncodable.swift
@@ -0,0 +1,22 @@
+//
+//  MLNDataEncodable.swift
+//  Melon Fashion SDK
+//
+//  Created by Dimka Novikov on 20.12.2024.
+//  Copyright © 2024 Melon Fashion Group. All rights reserved.
+//
+
+
+// MARK: Import section
+
+import Foundation
+
+
+
+// MARK: - MLNDataEncodable
+
+///
+///
+///
+@available(iOS 16.0, *)
+public typealias MLNDataEncodable = Encodable
