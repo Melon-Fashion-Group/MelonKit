de0a2c7536949b68c91478f5cd3fdfadae8a2ffe
Dmitry Novikov <nvkv.dmitry@gmail.com>
2024-12-22 13:51:44 +0300

    feat: add color ext

---
+++ Sources/MelonKit/Types Extensions/Advanced Types/Color/Protocols/ColorConvertible.swift
@@ -0,0 +1,30 @@
+//
+//  ColorConvertible.swift
+//  Melon Fashion SDK
+//
+//  Created by Dimka Novikov on 22.12.2024.
+//  Copyright © 2024 Melon Fashion Group. All rights reserved.
+//
+
+
+// MARK: Import section
+
+import SwiftUI
+
+
+
+// MARK: - ColorConvertible
+
+@available(iOS 16.0, *)
+protocol ColorConvertible {
+
+    // MARK: - Init
+
+    init?(hex hexString: String)
+
+
+
+    // MARK: - Public methods
+
+    func toHexString(isAlphaChannelNeeded: Bool) -> String?
+}
