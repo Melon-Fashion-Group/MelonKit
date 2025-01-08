57c95b5ecafd66218c9c0d0431a489f18c03cba7
Dmitry Novikov <nvkv.dmitry@gmail.com>
2025-01-08 21:50:00 +0300

    feat: add toast store

---
+++ Sources/MelonKit/App Layer/Navigation/Popovers/Toast/Enumerations/MLNToastPlacement.swift
@@ -0,0 +1,33 @@
+//
+//  MLNToastPlacement.swift
+//  Melon Fashion SDK
+//
+//  Created by Dimka Novikov on 08.01.2025.
+//  Copyright © 2025 Melon Fashion Group. All rights reserved.
+//
+
+
+// MARK: Import section
+
+import SwiftUI
+
+
+
+// MARK: - MLNToastPlacement
+
+///
+///
+///
+@available(iOS 17.0, *)
+public enum MLNToastPlacement {
+
+    ///
+    ///
+    ///
+    case top
+
+    ///
+    ///
+    ///
+    case bottom
+}
