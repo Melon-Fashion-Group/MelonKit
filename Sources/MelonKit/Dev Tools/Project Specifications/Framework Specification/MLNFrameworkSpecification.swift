4ebee534d948546da1f4528da0f53ae4fc3daad7
Dmitry Novikov <nvkv.dmitry@gmail.com>
2025-05-07 13:15:03 +0300

    refactor: rename task queue to simultaneous task queue

--- Sources/MelonKit/Dev Tools/Project Specifications/Framework Specification/MLNFrameworkSpecification.swift
+++ Sources/MelonKit/Dev Tools/Project Specifications/Framework Specification/MLNFrameworkSpecification.swift
@@ -26,7 +26,7 @@ public final class MLNFramework: MLNFrameworkSpecificationable {
     ///
     ///
     ///
-    public let version = Version(major: 1, minor: 4, patch: 0)
+    public let version = Version(major: 1, minor: 4, patch: 1)
 
     ///
     ///
